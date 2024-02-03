b3a0a6e5873622a43e46f5fb6503c26ba71f9cca
Georgiy Kamnev <nt.gocha@gmail.com>
2024-02-03 16:54:15 +0500

    inject custom attr into method ast

--- src/test/html/unit.js
+++ src/test/html/unit.js
@@ -6,10 +6,41 @@ const Counter = {
     }
   },
   created(){
-    //console.log('aaa')
     this.unit = unitData
   }
 }
 
 let app = Vue.createApp(Counter);
-app.mount('#app');
+app
+  .component('doc-comments',{
+    props: ['comments'],
+    template: '#doc-comments'
+  })
+  .component('doc-fun-returns',{
+    props: ['returns'],
+    template: '#doc-fun-returns'
+  })
+  .component('doc-arg',{
+    props: ['index','arg'],
+    template: '#doc-arg'
+  })
+  .component('doc-args',{
+    props: ['arguments'],
+    template: '#doc-args'
+  })
+  .component('doc-method',{
+    props: ['method'],
+    template: '#doc-method',
+    computed: {
+      cssClass() {        
+        return {
+          fun: this.method['@type'] == 'Function',
+          proc: this.method['@type'] == 'Procedure',
+          ctor: this.method['@type'] == 'Constructor',
+          dtor: this.method['@type'] == 'Destructor',
+          otor: this.method['@type'] == 'Operator',
+        }
+      }
+    }
+  })
+  .mount('#app');
